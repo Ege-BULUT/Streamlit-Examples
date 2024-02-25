ab556e60755f3897f7ea9c898af5d5f3badc3100
Ege BULUT <egebulut.iletisim@outlook.com>
2024-02-26 01:03:50 +0300

    configure() functions : to set page config  - page layout  - page icon  - page title  - initial sidebar state
     main(): main hierarchy

--- pages/9_🔐_Login.py
+++ pages/9_🔐_Login.py
@@ -1,8 +1,17 @@
 import streamlit as st
+import time
 
-# This code reloads saved variables values from session state
-for k in st.session_state.keys():
-    st.session_state[k] = st.session_state[k]
+def configure():
+    # This code reloads saved variables values from session state
+    for k in st.session_state.keys():
+        st.session_state[k] = st.session_state[k]
+
+    st.set_page_config(
+        layout="wide",
+        page_icon="🔐",
+        page_title="Login",
+        initial_sidebar_state="auto"
+    )
 
 def login():
     with st.columns(3)[1]:
@@ -11,27 +20,35 @@ def login():
         if st.button("log in"):
             # Connect to a db or local list of unames, upws and check
             # if user exists, login
-                user = {
-                    "name": username,
-                    "password" : pw
-                }
-                st.session_state.update({"user": user})
+            user = {
+                "name": username,
+                "password" : pw
+            }
+            st.session_state.update({"user": user})
+            st.rerun()
             # else display a warning
-
-
-
-
 
 def already_logged_in():
     user = st.session_state["user"]
-    st.write("You are already logged in as")
-    st.write(user["name"])
-    if st.button("log out"):
-        st.session_state.pop("user")
+    with st.columns([2, 4])[0]:
+        msg = st.success("Logged in successfully as " + user["name"] + " !")
+    c1, _, c2, _ = st.columns([2, 1, 2, 7])
+    with c1:
+        if st.button("log out"):
+            st.session_state.pop("user")
+            st.rerun()
+    with c2:
+        if st.button("Home Page"):
+            st.switch_page("0_🏠_WelcomePage.py")
 
 
-if __name__ == '__main__':
+def main():
     if "user" in st.session_state.keys() and st.session_state["user"]["name"] != "":
         already_logged_in()
     else:
         login()
+
+
+if __name__ == '__main__':
+    configure()
+    main()
